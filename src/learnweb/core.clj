8ea01f76030d8467f8c4a6d3aa8f48313d269b38
Kevin Jiang <kevin.jiang@lexisnexis.com>
2018-06-18 19:51:22 +0800

    使用Clojure开发Web入门系列（二）

--- src/learnweb/core.clj
+++ src/learnweb/core.clj
@@ -1,7 +1,27 @@
 (ns learnweb.core
+  (:require [ring.adapter.jetty :refer [run-jetty]]
+            [clojure.string :refer [split]])
   (:gen-class))
+
+(defn uri-split [str]
+  (filter #(not-empty %) (split str #"/")))    ; (1)
+
+(defn route [request]
+  (let [uri (:uri request)
+        seg (uri-split uri)]
+    (cond
+      (= uri "/") "Index page"
+      (= (first seg) "hello") (str "你好" (nth seg 1 "Oops"))
+      :else "Page not found")))    ; (2)
+
+(defn handler [request]
+  (let [uri (:uri request)
+        seg (uri-split uri)]
+    {:status 200
+     :headers {"Content-Type" "text/html; charset=utf-8"}
+     :body (route request)}))    ; (3)
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (run-jetty handler {:port 3000}))
