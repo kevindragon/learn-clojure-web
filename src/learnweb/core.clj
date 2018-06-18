63a78b0ded844f07e2ba9cc35b66cef143693824
Kevin Jiang <kevin.jiang@lexisnexis.com>
2018-06-18 18:24:08 +0800

    使用Clojure开发Web入门系列（一）

---
+++ src/learnweb/core.clj
@@ -0,0 +1,7 @@
+(ns learnweb.core
+  (:gen-class))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/learnweb/core_test.clj
@@ -0,0 +1,7 @@
+(ns learnweb.core-test
+  (:require [clojure.test :refer :all]
+            [learnweb.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
