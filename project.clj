69650f508fc0b78db1b1bc78258520c0a5c3c438
Kevin Jiang <kevin.jiang@lexisnexis.com>
2018-06-29 08:28:46 +0800

    add middlewares, html generator, reload

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [ring "1.6.3"]
-                 [compojure "1.6.1"]]
+                 [compojure "1.6.1"]
+                 [hiccup "1.0.5"]]
   :main ^:skip-aot learnweb.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/learnweb/core.clj
+++ src/learnweb/core.clj
@@ -1,16 +1,45 @@
 (ns learnweb.core
   (:require [ring.adapter.jetty :refer [run-jetty]]
-            [compojure.core :refer [defroutes GET]]    ; (1)
-            [compojure.route :refer [not-found]])    ; (2)
+            [ring.middleware.reload :refer [wrap-reload]]
+            [ring.middleware.params :refer [wrap-params]]
+            [compojure.core :refer [defroutes GET POST]]
+            [compojure.route :refer [not-found]]
+            [hiccup.core :refer [html]]
+            [hiccup.page :refer [html5]])
   (:gen-class))
+
+(def form-html
+  (html5
+    (html
+      [:body
+       [:h1 "Hello form"]
+       [:form {:action "/message" :method "post"}
+        [:div
+         [:label {:for "name"} "名字:"]
+         [:input {:type "text"
+                  :name "name"}]]
+        [:div
+         [:label {:for "message"} "消息:"]
+         [:textarea {:cols 80 :rows 5 :name "message"}]]
+        [:div
+         [:input {:type "submit" :value "提交"}]]]])))
+
+(defn show-message [params]
+  (let [{name "name" message "message"} params]
+    (html5
+      (html
+        [:body
+         [:h2 name]
+         [:p message]]))))
 
 (defroutes
   app
-  (GET "/" [] "<h1>Hello world!</h1>")
+  (GET "/" [] form-html)
+  (POST "/message" {params :params} (show-message params))
   (GET "/hello/:name" [name] (str "Hello " name))
-  (not-found "<h1>Page not found</h1>"))    ; (3)
+  (not-found "<h1>Page not found</h1>"))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (run-jetty app {:port 3000}))    ; (4)
+  (run-jetty (wrap-reload (wrap-params #'app)) {:port 3000}))
